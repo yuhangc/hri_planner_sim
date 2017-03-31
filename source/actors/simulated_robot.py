f6f8d14b3ddd5b5d829040adcc90124a4a71157a
Yuhang Che <cccumg@gmail.com>
2017-03-31 14:37:39 -0700

    added and tested a simulated deterministicd robot

---
+++ source/actors/simulated_robot.py
@@ -0,0 +1,48 @@
+from actor_base import Actor
+from source.planner.utils import *
+
+
+class SimulatedDetRobot(Actor):
+    def __init__(self, fp_radius=0.2, properties=None):
+        Actor.__init__(self, fp_radius)
+
+        # do not follow trajectory
+        self.set_follow_trajectory(False)
+
+        # current velocity
+        self.vel_curr = np.array([0.0, 0.0])
+        self.vel_goal = np.array([0.0, 0.0])
+
+        # set properties
+        if properties is None:
+            self.acc_max = np.array([0.5, 1.0])
+            self.vel_dead_zone = np.array([0.02, 0.05])
+        else:
+            pass
+
+    def set_velocity(self, vel_goal_new):
+        self.vel_goal = vel_goal_new
+
+    def update(self, dt=0.0):
+        """
+        Update dynamics, should be called in high frequency (> 500Hz)
+        """
+        self.t_curr += dt
+
+        # update position first
+        self.x, self.y, self.th = motion_update_2d((self.x, self.y, self.th),
+                                                   self.vel_curr,
+                                                   dt)
+
+        # update velocity
+        vel_diff = self.vel_goal - self.vel_curr
+        vel_inc_max = self.acc_max * dt
+        for k in range(2):
+            if np.abs(vel_diff[k]) > self.vel_dead_zone[k]:
+                # try to reach gaol velocity
+                if vel_diff[k] < vel_inc_max[k]:
+                    self.vel_curr[k] -= vel_inc_max[k]
+                elif vel_diff[k] > vel_inc_max[k]:
+                    self.vel_curr[k] += vel_inc_max[k]
+                else:
+                    self.vel_curr[k] = self.vel_goal[k]
--- source/actors/test_graphics.py
+++ source/actors/test_graphics.py
@@ -1,9 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+from time import sleep
 from actor_base import Actor
+from simulated_robot import SimulatedDetRobot
 
-if __name__ == "__main__":
+
+def test_actor_plotting():
     # test the plotting
     actor = Actor()
     traj = np.array([[0.0, 1.0, 1.0, 0.0],
@@ -27,3 +30,47 @@ if __name__ == "__main__":
         plt.pause(0.05)
 
         actor.update(dt)
+
+
+def test_simulated_det_robot():
+    robot = SimulatedDetRobot()
+
+    # create a plot axis
+    fig, ax = plt.subplots()
+    ax.set_aspect('equal')
+    ax.set_xlim(-3, 3)
+    ax.set_ylim(-3, 3)
+    plt.ion()
+
+    # motion list
+    cmd_vel_list = np.array([[0.0, 0.0, 0.0],
+                             [0.5, 0.5, 0.0],
+                             [2.5, 0.3, 0.5],
+                             [5.0, 0.0, -1.0],
+                             [7.0, 0.0, 0.0]])
+
+    # simulation at 1000hz
+    t_curr = 0.0
+    dt = 0.001
+    vel_idx = 0
+    for k in range(8000):
+        # update cmd_vel
+        if vel_idx < len(cmd_vel_list) and t_curr >= cmd_vel_list[vel_idx, 0]:
+            robot.set_velocity(cmd_vel_list[vel_idx, 1:])
+            vel_idx += 1
+
+        # update robot state
+        robot.update(dt)
+        sleep(dt * 0.2)
+
+        # plot at lower frequency
+        if k % 50 == 0:
+            robot.plot(ax)
+            plt.pause(dt * 0.5)
+
+        # update simulation time
+        t_curr += dt
+
+if __name__ == "__main__":
+    # test_actor_plotting()
+    test_simulated_det_robot()
